967a25e26ac34a7d034aa229aae0e5a9e4c3d5b9
onkar <onkar@pop-os.localdomain>
2023-07-16 14:25:56 +1000

    added installion script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+init=("update" "upgrade")
+applications=("neofetch" "gnome-tweaks" "code" "vlc")
+curlApplications=( "https://get.sdkman.io" "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh" )
+
+for x in ${init[@]};
+do
+	sudo apt-get "$x"
+done
+
+
+for app in ${applications[@]};
+do
+	sudo apt install "$app"
+done
+
+for app in ${curlApplications[@]};
+do
+	curl -s "$app" | bash
+done
+
+curl -s "" | bash
+curl -o-  | bash
